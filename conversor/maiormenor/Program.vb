f29085dd6a8c2bc9f6a7ce4f2d2b4f05fd91e3e1
julinho-hash <juliorodrigues2018@gmail.com>
2026-03-19 21:06:09 -0300

    código imcompleto

---
+++ conversor/maiormenor/Program.vb
@@ -0,0 +1,27 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        
+        Dim primeirovalor As integer
+        Dim segundovalor As integer
+        Dim resultado As integer
+
+        Console.WriteLine("digite seu primeiro valor")
+        primeirovalor = Console.ReadLine()
+
+
+        Console.WriteLine("digite seu segundo valor")
+        segundovalor = Console.ReadLine()
+
+
+        if primeiro valor maior segundo valor
+
+        then Console.WriteL
+
+        
+
+
+
+    End Sub
+End Module
