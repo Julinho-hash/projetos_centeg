f97ec933f1de8f4ad4dbac8a9abb57aa92770211
julinho-hash <juliorodrigues2018@gmail.com>
2026-03-24 20:48:00 -0300

    segundo deploy

---
+++ sistemas/Program.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        
+        Dim sistemas(3) As String
+        sistemas(0) = "Linux"
+        sistemas(1) = "Windows"
+        sistemas(2) = "IOS"
+
+        Console.WriteLine(sistemas(0))
+        Console.WriteLine(sistemas(1))
+        Console.WriteLine(sistemas(2))
+
+        
+
+
+
+
+    End Sub
+End Module
