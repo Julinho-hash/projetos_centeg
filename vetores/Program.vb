3b819e268307d368aae14a798c40c569384add94
julinho-hash <juliorodrigues2018@gmail.com>
2026-03-24 20:20:36 -0300

    primeiro deploy

---
+++ vetores/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        
+        Dim nomecompleto(2) As String
+        nomecompleto(0) = "Julio"
+        nomecompleto(1) = "Tavares"
+
+        Console.WriteLine(nomecompleto(0))
+        Console.WriteLine(nomecompleto(1))
+        
+
+        
+
+
+    End Sub
+End Module
